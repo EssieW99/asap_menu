62eb57fcc8de6eff5cdcd71f73d0b64e319a7c84
mwangii <jlmwangi254@gmail.com>
2024-05-24 17:13:15 +0300

    check frontend

---
+++ api/frontend_routes.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/menu2')
+def menu2():
+    """display other menu"""
+    return render_template('restaurant.html')
+
+@app.route('/menu3')
+def menu3():
+    """display third menu"""
+    return render_template('wedding.html')
+
+@app.route('/menu4')
+def menu4():
+    """catering menu"""
+    return render_template('catering.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
